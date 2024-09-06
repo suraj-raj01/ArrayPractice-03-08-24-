6f9038d513bb753bb7cd5f65a80a81aacea870e9
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:26:43 +0530

    Find Maximum Element:

--- src/arraypractice/example/question4/MyMain.java
+++ src/arraypractice/example/question4/MyMain.java
@@ -1,2 +1,16 @@
-package arraypractice.example.question4;public class MyMain {
+package arraypractice.example.question4;
+// Find Maximum Element:
+public class MyMain {
+    public static void main(String[] args) {
+        int[] arr = {5, 12, 3, 19, 7, 26, 14};
+        int max = arr[0];
+        for (int val : arr) {
+            if (val > max) {
+                max = val;
+            }
+        }
+        System.out.println("Max element :  "+max);
+    }
 }
+
+// Max element :  26
