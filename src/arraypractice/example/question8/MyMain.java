2eb3c75d2c881068ee4cdd8111beba600266958c
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:28:36 +0530

    Copying Arrays:

--- src/arraypractice/example/question8/MyMain.java
+++ src/arraypractice/example/question8/MyMain.java
@@ -1,2 +1,21 @@
-package arraypractice.example.question8;public class MyMain {
+package arraypractice.example.question8;
+// Copying Arrays:
+public class MyMain {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = new int[arr1.length];
+        for(int i=0; i<arr1.length; i++){
+            arr2[i] = arr1[i];
+        }
+        System.out.print("Second Array : [");
+        for(int i=0; i<arr2.length; i++){
+            System.out.print(arr2[i]);
+            if(i<arr2.length-1){
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
 }
+
+// Second Array : [1, 2, 3, 4, 5]
