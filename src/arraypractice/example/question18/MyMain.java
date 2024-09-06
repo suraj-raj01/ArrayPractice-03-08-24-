aaee962fbe534b23697d5b3437b19ac5859a6903
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:32:33 +0530

    Product of Array Elements:

--- src/arraypractice/example/question18/MyMain.java
+++ src/arraypractice/example/question18/MyMain.java
@@ -1,2 +1,14 @@
-package arraypractice.example.question18;public class MyMain {
+package arraypractice.example.question18;
+// Product of Array Elements:
+public class MyMain {
+    public static void main(String[] args) {
+        double[] arr = {1.5, 2.0, 3.5};
+        double product = 1;
+        for(double val:arr){
+            product*=val;
+        }
+        System.out.println("Product : "+product);
+    }
 }
+
+// Product : 10.5
