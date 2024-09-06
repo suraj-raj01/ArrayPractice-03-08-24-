91d58dbf37fd31b52f40e45a19866663d5566809
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:31:27 +0530

    Sum of Odd Elements:

--- src/arraypractice/example/question15/MyMain.java
+++ src/arraypractice/example/question15/MyMain.java
@@ -1,2 +1,16 @@
-package arraypractice.example.question15;public class MyMain {
+package arraypractice.example.question15;
+// Sum of Odd Elements:
+public class MyMain {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8};
+        int sum = 0;
+        for(int val:arr){
+            if(val%2!=0){
+                sum+=val;
+            }
+        }
+        System.out.println("Sum of Odd number : "+sum);
+    }
 }
+
+// Sum of Odd number : 16
