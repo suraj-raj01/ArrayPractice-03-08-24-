d37aa436ff5766efe17048d9ae4a1ef1d0ba50d6
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:32:12 +0530

    Find the Smallest Positive Number:

--- src/arraypractice/example/question17/MyMain.java
+++ src/arraypractice/example/question17/MyMain.java
@@ -1,2 +1,16 @@
-package arraypractice.example.question17;public class MyMain {
+package arraypractice.example.question17;
+// Find the Smallest Positive Number:
+public class MyMain {
+    public static void main(String[] args) {
+        int[] arr = {12, -5, 0, 8, 3};
+        int smallestPositive = arr[0];
+        for (int j : arr) {
+            if (smallestPositive > j && j > 0) {
+                smallestPositive = j;
+            }
+        }
+        System.out.println("Smallest Positive : "+smallestPositive);
+    }
 }
+
+// Smallest Positive : 3
