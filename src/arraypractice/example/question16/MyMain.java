fc41ca9ce7c07600cabdb17aa2ae6a1e6284c216
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:31:51 +0530

    Replace Elements in an Array:

--- src/arraypractice/example/question16/MyMain.java
+++ src/arraypractice/example/question16/MyMain.java
@@ -1,2 +1,19 @@
-package arraypractice.example.question16;public class MyMain {
+package arraypractice.example.question16;
+// Replace Elements in an Array:
+public class MyMain {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 4, 7};
+        // replace 4 with 9
+        int replace = 4;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==4){
+                arr[i] = 9;
+            }
+        }
+        for(int val:arr){
+            System.out.print(val+" ");
+        }
+    }
 }
+
+// 9 5 6 9 7
