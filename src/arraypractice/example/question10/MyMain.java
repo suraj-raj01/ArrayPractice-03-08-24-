1ca9b548797dff4ac30b54ff9375ce943b9f14cb
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:29:31 +0530

    Count Occurrences of an Element:

--- src/arraypractice/example/question10/MyMain.java
+++ src/arraypractice/example/question10/MyMain.java
@@ -1,2 +1,24 @@
-package arraypractice.example.question10;public class MyMain {
+package arraypractice.example.question10;
+// Count Occurrences of an Element:
+public class MyMain {
+    public static void main(String[] args) {
+        char[] arr = {'a', 'b', 'a', 'c', 'a', 'd'};
+        int t = 0;
+        char temp = '0';
+        for (char value : arr) {
+            int c = 0;
+            for (char item : arr) {
+                if (value == item) {
+                    c++;
+                }
+            }
+            if (t < c) {
+                t = c;
+                temp = value;
+            }
+        }
+        System.out.println("'"+temp+ "' occurs "+t+" times");
+    }
 }
+
+// 'a' occurs 3 times
