d1d02fe90dc2bcb1315abe901a14ea9073034a50
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:30:43 +0530

    Array of Floating-point Numbers:

--- src/arraypractice/example/question13/MyMain.java
+++ src/arraypractice/example/question13/MyMain.java
@@ -1,2 +1,17 @@
-package arraypractice.example.question13;public class MyMain {
+package arraypractice.example.question13;
+// Array of Floating-point Numbers:
+public class MyMain {
+    public static void main(String[] args) {
+        float[] arr= {1.1f, 2.2f, 3.3f, 4.4f};
+        System.out.print("[");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]);
+            if(i<arr.length-1){
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
 }
+
+// [1.1, 2.2, 3.3, 4.4]
