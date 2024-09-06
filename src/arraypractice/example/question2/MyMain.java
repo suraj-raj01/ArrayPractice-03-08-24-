816860b83fc0b119b21bff724b9e391ca631cdc4
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:24:09 +0530

    Sum of Array Elements:

--- src/arraypractice/example/question2/MyMain.java
+++ src/arraypractice/example/question2/MyMain.java
@@ -1,2 +1,14 @@
-package arraypractice.example.question2;public class MyMain {
+package arraypractice.example.question2;
+// Sum of Array Elements:
+public class MyMain {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int sum = 0;
+        for(int val:arr){
+            sum+=val;
+        }
+        System.out.println("Sum : "+sum);
+    }
 }
+
+//Sum : 55
