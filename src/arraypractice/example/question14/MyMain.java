2a7f2a1e09b1e1905362d8f5620833c500dc1ffa
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:31:06 +0530

    Check for Duplicates:

--- src/arraypractice/example/question14/MyMain.java
+++ src/arraypractice/example/question14/MyMain.java
@@ -1,2 +1,19 @@
-package arraypractice.example.question14;public class MyMain {
+package arraypractice.example.question14;
+// Check for Duplicates:
+public class MyMain {
+    public static void main(String[] args) {
+        int[] arr = {7, 3, 9, 2, 3, 8};
+        int count=0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length-1; j++){
+                if(arr[i]==arr[j+1]){
+                    count++;
+                }
+            }
+        }
+        if(count>2){
+            System.out.println("yes");
+        }
+        System.out.println(count);
+    }
 }
