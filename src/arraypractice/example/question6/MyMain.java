98b1b3344b1700326e459b55dedf8810b0d6723c
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:27:42 +0530

    Reverse an Array:

--- src/arraypractice/example/question6/MyMain.java
+++ src/arraypractice/example/question6/MyMain.java
@@ -1,2 +1,17 @@
-package arraypractice.example.question6;public class MyMain {
+package arraypractice.example.question6;
+// Reverse an Array:
+public class MyMain {
+    public static void main(String[] args) {
+        char[] character = {'a', 'b', 'c', 'd', 'e'};
+        System.out.print("[");
+        for(int i = character.length-1; i>=0; i--){
+            System.out.print(character[i]);
+            if(i>0){
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
 }
+
+// [e, d, c, b, a]
