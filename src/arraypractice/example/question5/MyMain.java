f7c24bd4f9dc7e7093c1fd25cc40d9fc92c2669d
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:27:13 +0530

    Find Minimum Element:

--- src/arraypractice/example/question5/MyMain.java
+++ src/arraypractice/example/question5/MyMain.java
@@ -1,2 +1,16 @@
-package arraypractice.example.question5;public class MyMain {
+package arraypractice.example.question5;
+// Find Minimum Element:
+public class MyMain {
+    public static void main(String[] args) {
+        double[] arr = {2.5, 3.8, 1.9, 5.6, 4.3};
+        double min = arr[0];
+        for(double val:arr){
+            if(min>val){
+                min = val;
+            }
+        }
+        System.out.println("Minimum value : "+min);
+    }
 }
+
+// Minimum value : 1.9
