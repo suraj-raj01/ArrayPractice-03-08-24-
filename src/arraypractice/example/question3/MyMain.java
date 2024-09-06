9a20731d9c80bac0bf2cf4e01ecef71004b9f86c
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:25:32 +0530

    Average of Array Elements:

--- src/arraypractice/example/question3/MyMain.java
+++ src/arraypractice/example/question3/MyMain.java
@@ -1,2 +1,16 @@
-package arraypractice.example.question3;public class MyMain {
+package arraypractice.example.question3;
+//Average of Array Elements:
+public class MyMain {
+    public static void main(String[] args) {
+        float[] arr = {1.5f, 2.5f, 3.5f, 4.5f, 5.5f, 6.5f, 7.5f};
+        float average = 0;
+        float sum = 0;
+        for(float val:arr){
+            sum+=val;
+        }
+        average = sum/arr.length;
+        System.out.printf("%f",average);
+    }
 }
+
+// 4.500000
