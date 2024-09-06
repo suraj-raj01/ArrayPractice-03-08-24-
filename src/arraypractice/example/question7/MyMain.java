60103f512998fd28dd03320321e6b16f12af499d
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-06 13:28:09 +0530

    Element Search in Array:

--- src/arraypractice/example/question7/MyMain.java
+++ src/arraypractice/example/question7/MyMain.java
@@ -1,2 +1,15 @@
-package arraypractice.example.question7;public class MyMain {
+package arraypractice.example.question7;
+// Element Search in Array:
+public class MyMain {
+    public static void main(String[] args) {
+        int[] arr = {4, 8, 15, 16, 23, 42};
+        int search = 15;
+        for(int i=0; i<arr.length; i++){
+            if(search==arr[i]){
+                System.out.println("Element "+arr[i]+" found at index "+i);
+            }
+        }
+    }
 }
+
+// Element 15 found at index 2
